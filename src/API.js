5d7a9f668992e37e48ce54c16f84fa94e635c17e
Amy Karnaze <akarnaze@gmail.com>
2020-09-09 17:44:51 -0600

    Create variables for url and key, add fetchAllBooks

--- src/API.js
+++ src/API.js
@@ -1,1 +1,11 @@
+const rootUrl = "https://api.nytimes.com/svc/books/v3/";
+const key = "api-key=x5EmADYf2iLxSpp6H6EiN4JnZCZ5RMps";
 
+export const fetchAllBooks = () => {
+  return fetch(`${rootUrl}lists/current/hardcover-fiction.json?${key}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+    })
+}
