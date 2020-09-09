ff317c86eb2ec842a4dd42292c0da31d1bbe67a1
Amy Karnaze <akarnaze@gmail.com>
2020-09-09 17:47:44 -0600

    Add Books component class, componentDidMount that sets the books state to resolved data, add render

--- src/Components/Books/Books.js
+++ src/Components/Books/Books.js
@@ -1,2 +1,27 @@
-import React from 'react'
-import './Books.scss'
+import React, { Component } from 'react'
+import './Books.css'
+import {fetchAllBooks} from '../../API'
+
+class Books extends Component {
+  constructor() {
+    super()
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    fetchAllBooks()
+      // .then(data => console.log(data))
+      .then(data => this.setState({books: data}))
+      .catch(error => alert(error.message))
+  }
+
+  render() {
+    return (
+      <section>Books!</section>
+    )
+  }
+}
+
+export default Books;
