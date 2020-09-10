b3bcbcc6f28509c4fd4f27b7a9e73a7530da4075
Amy Karnaze <akarnaze@gmail.com>
2020-09-10 09:28:18 -0600

    Add searchBooks method, pass to Search component and add foundBoods array to state

--- src/Components/Books/Books.js
+++ src/Components/Books/Books.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import './Books.css'
 import {fetchAllBooks} from '../../API'
 import Book from '../Book/Book'
+import Search from '../Search/Search'
 
 class Books extends Component {
   constructor() {
     super()
     this.state = {
-      books: []
+      books: [],
+      searchedBooks: []
     }
   }
 
@@ -26,12 +28,29 @@ class Books extends Component {
     })
   }
 
+  searchBooks = (search) => {
+    console.log("HERE", search)
+    // let split = search.split('')
+    // console.log("split", split)
+    let searchCase = search;
+
+    let foundBooks = this.state.books.filter(book => {
+      if (book.title.includes(searchCase) || book.author.includes(searchCase)) {
+        this.setState({searchedBooks: book})
+      }
+    })
+    console.log("found", this.state.searchedBooks)
+    console.log("foundBooks",foundBooks)
+    return foundBooks;
+  }
+
   render() {
     let bookCards = this.displayBooks()
     return (
-      
       <section>
+        <h3>{this.state.searchedBooks}/></h3>
         <h1>Books!</h1>
+        <Search searchBooks={this.searchBooks}/>
         <div className="books-container">{this.state.books && bookCards}</div>
       </section>
     )
