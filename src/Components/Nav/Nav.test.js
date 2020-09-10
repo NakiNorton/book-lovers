aae649540b915038dcc6adf5d4f4642e929d9541
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-09 21:42:33 -0600

    Write test for correct ui rendering

--- src/Components/Nav/Nav.test.js
+++ src/Components/Nav/Nav.test.js
@@ -1,3 +1,21 @@
 import React from 'react';
-import { screen, render, waitFor } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import Nav from './Nav'
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Nav component', () => {
+  it('should have the correct content when rendered', () => {
+
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    )
+
+    const homeLink = screen.getByText('HOME')
+    const readingListLink = screen.getByText('READING LIST')
+
+    expect(homeLink).toBeInTheDocument();
+    expect(readingListLink).toBeInTheDocument();
+  })
+})
