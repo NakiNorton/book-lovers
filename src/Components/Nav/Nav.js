c732a1080efc4dd4e3505246806c414b720e99d0
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-09 21:32:44 -0600

    Import and style book icon

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import './Nav.css'
-
+import bookIcon from '../../assets/book.png'
+import './Nav.css'  
 
 const Nav = () => {
   return (
     <nav className='Nav'>
+      <img className='icon' alt='book icon' src={bookIcon} />
       <label className='app-logo'>Book Lovers</label>
       <ul>
         <li><NavLink to='/' className='nav-link active' aria-label='navigate to home page'>HOME</NavLink></li>
