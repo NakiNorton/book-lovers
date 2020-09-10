1942e8be276b47c9c0aa453688e656cccc4e9be9
Amy Karnaze <akarnaze@gmail.com>
2020-09-09 18:30:57 -0600

    Add Book component to display books

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -1,3 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Books from './Books.css'
+import './Book.css'
+
+
+const Book = ({book}) => {
+  return (
+    <section>
+      <h1>{book.rank}</h1>
+    </section>
+  )
+}
+
+export default Book
