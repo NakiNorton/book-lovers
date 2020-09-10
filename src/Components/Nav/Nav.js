73ae768777b453d0e0aaf70f68372521f698ed6c
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-09 20:11:57 -0600

    Create nav component with basic styling

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './Nav.css'
 
+
+const Nav = () => {
+  return (
+    <nav className='Nav'>
+      <label className='app-logo'>Book Lovers</label>
+      <ul>
+        <li><NavLink to='/' className='nav-link active' aria-label='navigate to home page'>HOME</NavLink></li>
+        <li><NavLink to='/' className='nav-link active' aria-label='navigate to reading list'>READING LIST</NavLink></li>
+      </ul>
+    </nav>
+  )
+}
+
 export default Nav
+
