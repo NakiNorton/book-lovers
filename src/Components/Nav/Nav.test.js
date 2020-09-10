4043e5712eceefb5c20189ec3082c5b0db5ebc92
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-09 21:52:10 -0600

    Write outline for testing of nav links

--- src/Components/Nav/Nav.test.js
+++ src/Components/Nav/Nav.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import Nav from './Nav'
 import { MemoryRouter } from 'react-router-dom';
 
@@ -17,5 +17,37 @@ describe('Nav component', () => {
 
     expect(homeLink).toBeInTheDocument();
     expect(readingListLink).toBeInTheDocument();
+  })
+
+
+  // TESTS TO BE FINISHED ONCE COMPONENTS ARE CONNECTED AND RENDERING ----
+  it('should take the user to the homepage when the home button is clicked', () => {
+
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    )
+
+    // const homeLink = screen.getByText('HOME')
+    // fireEvent.click(homeLink)
+
+    // const homeHeading = screen.getByText('Hiyas')
+    //  expect(homeHeading).toBeInTheDocument()
+
+  })
+
+  it('should take the user to the reading list page when the reading list button is clicked', () => {
+
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    )
+
+    // const readingListLink = screen.getByText('READING LIST')
+
+    // const readingListHeading = screen.getByText('')
+    // expect(readingListHeading).toBeInTheDocument()
   })
 })
