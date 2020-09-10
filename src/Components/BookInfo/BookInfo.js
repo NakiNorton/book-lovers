ba470ee6ce9e6692763a93bae74c78cd1f29a63d
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-09 19:39:51 -0600

    Add initial book details elements

--- src/Components/BookInfo/BookInfo.js
+++ src/Components/BookInfo/BookInfo.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react'
 import './BookInfo.css'
 
+const BookInfo = ({ book }) => {
+  return (
+    <section class="book-card-section">
+        <h1>{book.title}</h1>
+        <h3>{book.author}</h3>
+        <h3>Ranking: {book.rank}</h3>
+        <p>{book.description}</p>
+        <img className="Book-card-image" alt="Book cover" src={book.book_image} />
+    </section>
+  )
+}
 
 export default BookInfo
