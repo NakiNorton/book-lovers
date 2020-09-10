a89687218cc120e5aac4b883756a8d4f9f1b24b6
Jake West <jacobkwest93@gmail.com>
2020-09-09 21:06:07 -0600

    Add active and inactive styling for buttons #10

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import './Book.css'
 
 
-const Book = ({book, addBook}) => {
+const Book = ({book, addBook, isOnList}) => {
   return (
     <section class="book-card-section">
         <h1>{book.title}</h1>
         <h3>{book.author}</h3>
         <h3>Ranking: {book.rank}</h3>
         <img className="Book-card-image" alt="Book cover" src={book.book_image} />
-        <button id={book.primary_isbn10} onClick={addBook}>Add to Reading List</button>
+        <button id={book.primary_isbn10} onClick={addBook} className="reading-list-button active">
+          Reading List
+        </button>
     </section>
   )
 }
