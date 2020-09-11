720f7d1f3119cc03d482d7d0834301097502e24e
Jake West <jacobkwest93@gmail.com>
2020-09-10 20:03:39 -0600

    Change App back to original styling #18

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,46 +2,15 @@ import React, { Component } from 'react'
 import './App.css';
 import Books from '../Books/Books'
 import Nav from '../Nav/Nav'
-import { connect } from 'react-redux'
-import { Route } from 'react-router-dom';
-import { setBooks } from '../../actions';
-import {fetchAllBooks} from '../../API';
-import { bindActionCreators } from 'redux';
 
-class App extends Component {
-  async componentDidMount() {
-    const { setBooks } = this.props;
-    try {
-      const books = await fetchAllBooks()
-      console.log(books.results.books)
-      console.log(setBooks)
-      setBooks(books.results.books)
-    }
-    catch ({ message }) {
-      alert(message);
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Nav />
-       <h1>Hiyas</h1>
-       <Books />
-      </div>
-    );
-  }
+function App () {
+  return (
+    <div className="App">
+      <Nav />
+      <Books />
+    </div>
+  );
 }
 
-export const mapStateToProps = ({ books }) => ({
-  books,
-})
-
-export const mapDispatchToProps = dispatch => (
-  bindActionCreators({
-    setBooks
-  }, dispatch)
-)
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
  
