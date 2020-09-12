6c3d7c118363b9f219690c3adc26160768b1adb5
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-12 13:47:59 -0600

    Import removeFavorite action and call it in the handleRemoveClick function

--- src/Components/ReadingListBook/ReadingListBook.js
+++ src/Components/ReadingListBook/ReadingListBook.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './ReadingListBook.css'
+import { removeFavorite } from '../../actions'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
 
-const ReadingListBook = ({ title, image, id, removeBook }) => {
+const ReadingListBook = ({ title, image, id, removeFavorite }) => {
+  const handleRemoveClick = (event) => {
+    removeFavorite(event.target.id)
+  }
 
   return (
     <article className='book-card'>
       <img className="card-image" alt={title} src={image} />
-      <button className='remove-book' id={id} onClick={removeBook}>Remove</button>
+      <button className='remove-book' id={id} onClick={(event) => handleRemoveClick(event)}>Remove</button>
     </article>
   )
 }
 
-export default ReadingListBook
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({ removeFavorite }, dispatch)
+)
+
+export default connect(null, mapDispatchToProps)(ReadingListBook);
