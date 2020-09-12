223826ce7d324a1b4185111baeab04a6cfb46c8f
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-12 10:22:07 -0600

    Remove console logs and fix spacing and indentation

--- src/reducers/ReadingList.js
+++ src/reducers/ReadingList.js
@@ -1,12 +1,7 @@
 export const readingList = (state = [], action) => {
-  console.log('state 1:', state)
-  console.log('action:', action)
   switch (action.type) {
     case 'SET_READING_LIST':
-      
       return ( [...state, action.readingList] )
-
-
     default:
       return state
   }
