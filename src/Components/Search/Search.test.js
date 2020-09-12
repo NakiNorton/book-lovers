14bf8ea8d79c31f7df8adcbf0ad04ff9c315d974
Amy Karnaze <akarnaze@gmail.com>
2020-09-12 16:03:48 -0600

    Add test to see if search bar is rendered

--- src/Components/Search/Search.test.js
+++ src/Components/Search/Search.test.js
@@ -1,4 +1,17 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { screen, render, waitFor } from '@testing-library/react'
+import { screen, render, waitFor, fireEvent } from '@testing-library/react'
 import Search from './Search.js'
+jest.mock('../../API');
+
+describe('Search Componey', () => {
+  it('should render a search bar and button', () => {
+    render(<Search />);
+
+    const searchInput = screen.getByPlaceholderText('search')
+    const submitButton = screen.getByRole('button', {name: 'Search'})
+
+    expect(searchInput).toBeInTheDocument()
+    expect(submitButton).toBeInTheDocument()
+  })
+})
