4dc286cd7d4c8fa5b28ab55744f9bab5d6452e0a
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-12 10:08:35 -0600

    Fix bug to enable book  to be added to state store

--- src/reducers/ReadingList.js
+++ src/reducers/ReadingList.js
@@ -1,10 +1,10 @@
 export const readingList = (state = [], action) => {
-  console.log('state:', state)
+  console.log('state 1:', state)
   console.log('action:', action)
   switch (action.type) {
     case 'SET_READING_LIST':
       
-      return { ...state, readingList: action.readingList }
+      return ( [...state, action.readingList] )
 
 
     default:
