03529217fed99fb195bc88fbcf454adc3947a23f
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-13 09:38:24 -0600

    Refactor tests to work with readingList state from store

--- src/Components/ReadingList/ReadingList.test.js
+++ src/Components/ReadingList/ReadingList.test.js
@@ -2,32 +2,24 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import ReadingList from '../ReadingList/ReadingList'
 import { MemoryRouter } from 'react-router-dom';
-
+import '@testing-library/jest-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from '../../reducers';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('ReadingList component', () => {
-  let readingList;
-  beforeEach(() => {
-    readingList = [
-      {
-        'description': 'In a quiet town...',
-        'title': 'WHERE THE CRAWDADS SING',
-        'author': 'Delia Owens',
-        'book_image': 'https://s1.nyt.com/du/books/images/9780735219090.jpg',
-        'amazon_product_url': 'https://www.amazon.com/Where-Crawdads-Sing-Delia-Owens/dp/0735219095?tag=NYTBS-20',
-      }
-    ]
-  })
-
-  it('should display the correct content when rendered', () => {
+  it('should display the page heading', () => {
     const store = createStore(rootReducer);
-
+    
     render(
       <Provider store={store}>
         <MemoryRouter>
-          <ReadingList toReadList={readingList} />
+          <ReadingList />
         </MemoryRouter>
       </Provider>
     )
@@ -37,12 +29,21 @@ describe('ReadingList component', () => {
   })
 
   it('should display books saved to the reading list', () => {
-    const store = createStore(rootReducer);
-
+    let store = mockStore({
+      readingList: [
+        {
+          'description': 'In a quiet town...',
+          'title': 'WHERE THE CRAWDADS SING',
+          'author': 'Delia Owens',
+          'book_image': 'https://s1.nyt.com/du/books/images/9780735219090.jpg',
+          'amazon_product_url': 'https://www.amazon.com/Where-Crawdads-Sing-Delia-Owens/dp/0735219095?tag=NYTBS-20',
+        }
+      ]})
+ 
     render(
       <Provider store={store}>
         <MemoryRouter>
-          <ReadingList toReadList={readingList} />
+          <ReadingList />
         </MemoryRouter>
       </Provider>
     )
@@ -55,12 +56,11 @@ describe('ReadingList component', () => {
 describe('ReadingList component', () => {
   it('should display a message if there are no books to display', () => {
     const store = createStore(rootReducer);
-    const readingList = [];
-
+   
     render(
       <Provider store = { store } >
         <MemoryRouter>
-          <ReadingList toReadList={readingList} />
+          <ReadingList />
         </MemoryRouter>
       </Provider>
     )
