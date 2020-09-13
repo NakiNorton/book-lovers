32f029e6e07fd842e3fcb8184c6b6601065dde35
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-13 10:12:53 -0600

    Refactor tests to work with mock prop from store

--- src/Components/ReadingListBook/ReadingListBook.test.js
+++ src/Components/ReadingListBook/ReadingListBook.test.js
@@ -1,50 +1,31 @@
 import React from 'react';
-import { screen, render, fireEvent, waitFor} from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ReadingListBook from '../ReadingListBook/ReadingListBook'
 import { MemoryRouter } from 'react-router-dom';
 import { removeFavorite } from '../../actions'
-
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { rootReducer } from '../../reducers';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
-import configureStore from 'redux-mock-store';
-const mockStore = configureStore([]);
-
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('ReadingListBook component', () => {
-  it('should display the correct content when rendered', () => {
-    const store = createStore(rootReducer);
-
-    render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <ReadingListBook
-            title={'WHERE THE CRAWDADS SING'}
-            image={'https://s1.nyt.com/du/books/images/9780735219090.jpg'}
-            id={'015489213'}
-            key={'015489213'}
-          />
-        </MemoryRouter>
-      </Provider>
-    )
-
-    const bookTitle = screen.getByAltText('WHERE THE CRAWDADS SING')
-    const removeButton = screen.getByRole('button', {name: 'Remove'})
-  
-    expect(bookTitle).toBeInTheDocument();
-    expect(removeButton).toBeInTheDocument();
-  })
-})
-
-describe('My Connected React-Redux Component', () => {
   let store;
-  
   beforeEach(() => {
     store = mockStore({
-      myState: 'sample text',
-    });
+      readingList: [
+        {
+          'description': 'In a quiet town...',
+          'title': 'WHERE THE CRAWDADS SING',
+          'author': 'Delia Owens',
+          'book_image': 'https://s1.nyt.com/du/books/images/9780735219090.jpg',
+          'amazon_product_url': 'https://www.amazon.com/Where-Crawdads-Sing-Delia-Owens/dp/0735219095?tag=NYTBS-20',
+        }
+      ]
+      
+    })
 
     store.dispatch = jest.fn();
 
@@ -62,16 +43,24 @@ describe('My Connected React-Redux Component', () => {
     )
   })
 
-  it('should invoke a function to remove book when button is clicked', () => {
-      const removeButton = screen.getByRole('button', { name: 'Remove' })
-      fireEvent.click(removeButton)
+ it('should display the correct content when rendered', () => {
+    
+    const bookTitle = screen.getByAltText('WHERE THE CRAWDADS SING')
+    const removeButton = screen.getByRole('button', {name: 'Remove'})
+  
+    expect(bookTitle).toBeInTheDocument();
+    expect(removeButton).toBeInTheDocument();
+  })
+
+  it('should invoke a redux action to remove book when button is clicked', () => {
+
+    const removeButton = screen.getByRole('button', { name: 'Remove' })
+    fireEvent.click(removeButton)
 
     expect(store.dispatch).toHaveBeenCalledTimes(1);
     // expect(store.dispatch).toHaveBeenCalledWith(
-    //   removeFavorite({ payload: "015489213" })
+    //   removeFavorite( { payload: "015489213" })
     //   );
-    })
+  })
 })
-
-
   
