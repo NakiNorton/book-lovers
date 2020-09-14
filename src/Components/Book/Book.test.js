fa52a0fd64810573e31ca5fcf911d4d8e663e27f
Amy Karnaze <akarnaze@gmail.com>
2020-09-13 18:56:43 -0600

    Add book info is rendered to page in book component test

--- src/Components/Book/Book.test.js
+++ src/Components/Book/Book.test.js
@@ -1,3 +1,48 @@
-import React from 'react';
-import { screen, render, waitFor } from '@testing-library/react';
+import React from 'react'
+import '@testing-library/jest-dom'
+import { screen, render, waitFor, fireEvent } from '@testing-library/react'
 import Book from './Book'
+import Books from '../Books/Books'
+import { MemoryRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from '../../reducers';
+
+describe('Book Component', () => {
+  // let book;
+  beforeEach(() => {
+    
+  })
+  it('should render book information',() => {
+    const book1 = {
+      rank: 1 ,
+      primary_isbn10: "1250145236",
+      title: "ALL THE DEVILS ARE HERE",
+      book_image: "https://s1.nyt.com/du/books/images/9781250145239.jpg",
+      author: "Louise Penny"
+    }
+    const store = createStore(rootReducer);
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Book book={book1}/>
+        </MemoryRouter>
+      </Provider>
+    )
+
+    const bookRank = screen.getByText("1", { exact: false })
+    // const bookIsbn = screen.getByText("href=/1250145236", {exact: false})
+    const bookTitle = screen.getByText("ALL THE DEVILS ARE HERE")
+    const bookImg = screen.getByAltText("ALL THE DEVILS ARE HERE", { exact: false })
+    const bookAuthor = screen.getByText("Louise Penny")
+    const addBookButton = screen.getByRole("button", {name: "Reading List"})
+
+    expect(bookRank).toBeInTheDocument();
+    // expect(bookIsbn).toBeInTheDocument();
+    expect(bookTitle).toBeInTheDocument();
+    expect(bookImg).toBeInTheDocument();
+    expect(bookAuthor).toBeInTheDocument();
+    expect(addBookButton).toBeInTheDocument();
+  })
+})
