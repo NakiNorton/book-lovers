da45ad81aa60a4b48d971492323be9a4208e41e3
Amy Karnaze <akarnaze@gmail.com>
2020-09-13 19:43:17 -0600

    Add unit test for reading list button

--- src/Components/Book/Book.test.js
+++ src/Components/Book/Book.test.js
@@ -2,17 +2,12 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import { screen, render, waitFor, fireEvent } from '@testing-library/react'
 import Book from './Book'
-import Books from '../Books/Books'
 import { MemoryRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from '../../reducers';
 
 describe('Book Component', () => {
-  // let book;
-  beforeEach(() => {
-    
-  })
   it('should render book information',() => {
     const book1 = {
       rank: 1 ,
@@ -44,5 +39,27 @@ describe('Book Component', () => {
     expect(bookImg).toBeInTheDocument();
     expect(bookAuthor).toBeInTheDocument();
     expect(addBookButton).toBeInTheDocument();
+  })
+
+  it('Reading List button should be able to fire', () => {
+    const book1 = {
+      rank: 1 ,
+      primary_isbn10: "1250145236",
+      title: "ALL THE DEVILS ARE HERE",
+      book_image: "https://s1.nyt.com/du/books/images/9781250145239.jpg",
+      author: "Louise Penny"
+    }
+    const store = createStore(rootReducer);
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Book book={book1}/>
+        </MemoryRouter>
+      </Provider>
+    )
+    
+    const addBookButton = screen.getByRole("button", {name: "Reading List"})
+    fireEvent.click(addBookButton)
   })
 })
