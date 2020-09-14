05bc3072b20bde9c7b787a65033d4501e8d1232c
Jake West <jacobkwest93@gmail.com>
2020-09-14 14:53:23 -0600

    Add list name to App state w/ toggle; refactor componentDidMount & filterBooks #32

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,30 +2,62 @@ import React, { Component } from 'react'
 import './App.css';
 import Books from '../Books/Books'
 import Nav from '../Nav/Nav'
+import { fetchBooks } from '../../API';
+import { connect } from 'react-redux'
+import { setBooks, setList } from '../../actions';
+import { bindActionCreators } from 'redux';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       lists: {
-        'hardcover-fiction': true,
         'celebrities': true,
-        'food-and-fitness': true
+        'food-and-fitness': true,
+        'hardcover-fiction': true,
+        'games-and-activities': true,
+        'health': true,
       }
     }
   }
 
+  componentDidMount() {
+    const { setBooks, setList } = this.props
+    const allListUrls = Object.keys(this.state.lists)
+    try {
+      allListUrls.map(async url => {
+        const response = await fetchBooks(url);
+        setBooks(response.results.books);
+        const books = response.results.books;
+        console.log('books after setBooks', books)
+        return setList(url, books.map(book => book.primary_isbn10));
+      })
+    }
+    catch ({ message }) {
+      alert(message)
+    }
+  }
+
+  filterBooks = (listName) => {
+    const listOfIds = this.props.lists[listName]
+    const filteredBooks = this.props.books.filter(book => listOfIds.includes(book.primary_isbn10))
+    console.log('filtered books from filterBooks', filteredBooks)
+    return filteredBooks;
+  }
+
   createBookLists = () => {
-    const lists = this.state.lists;
-    const listsKeys = Object.keys(lists);
-    const allBooks = listsKeys.map(list => {
-      if(lists[list]) {
+    const listUrls = this.props.lists;
+    const listsKeys = Object.keys(listUrls);
+    const listBooks = listsKeys.map(listName => {
+      const filteredBooks = this.filterBooks(listName)
+      console.log('filtered books from createBookLists', filteredBooks)
+      if(filteredBooks.length > 0) {
         return (
-          <Books key={list} id={list} list={list} />
+          <Books key={listName} id={listName} listName={listName} filteredBooks={filteredBooks}/>
         )
       }
     })
-    return allBooks
+    return listBooks
   }
 
   render() {
@@ -38,5 +70,17 @@ class App extends Component {
   }
 }
 
-export default App;
+export const mapStateToProps = ({ books, lists }) => ({
+  books,
+  lists
+})
+
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    setBooks,
+    setList
+  }, dispatch)
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
  
