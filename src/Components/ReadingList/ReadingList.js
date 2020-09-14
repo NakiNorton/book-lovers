56dd7221089d89d4d0de0b1ed1d9ead93023bfe6
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-14 09:24:31 -0600

    Adjust indentation

--- src/Components/ReadingList/ReadingList.js
+++ src/Components/ReadingList/ReadingList.js
@@ -18,21 +18,21 @@ const ReadingList = ({ toReadList }) => {
     <section className='ReadingList'>
       <h1 className='page-title'>My Library</h1>
       {toReadList.length === 0 &&
-      <section>
-        <h3 className='no-books-msg'>You haven't added any books to your reading list yet. </h3>
-        <div className='ani-container'>
-          <div className='tear1 tear'></div>
-          <div className='tear2 tear'></div>
-          <div className='face'>
-            <div className='eyebrow'>︶</div>
-            <div className='eyebrow'>︶</div>
-            <div className='eye'></div>
-            <div className='eye'></div>
-            <div className='mouth'></div>
-          </div>
-        </div> 
+        <section>
+          <h3 className='no-books-msg'>You haven't added any books to your reading list yet. </h3>
+          <div className='ani-container'>
+            <div className='tear1 tear'></div>
+            <div className='tear2 tear'></div>
+            <div className='face'>
+              <div className='eyebrow'>︶</div>
+              <div className='eyebrow'>︶</div>
+              <div className='eye'></div>
+              <div className='eye'></div>
+              <div className='mouth'></div>
+            </div>
+          </div> 
         </section>
-        }
+      }
       <section className="reading-list-section">
         {booksToRead}
       </section>
