37a89401511fd517290c3d2217845a8e3d8efb8a
Jake West <jacobkwest93@gmail.com>
2020-09-15 15:11:15 -0600

    Add newState to lists reducer #59

--- src/reducers/Lists.js
+++ src/reducers/Lists.js
@@ -1,16 +1,17 @@
 const initState = {
     'celebrities': [],
     'food-and-fitness': [],
-    'hardcover-fiction': [],
-    'health': [],
-    'games-and-activities': [],
+    // 'hardcover-fiction': [],
+    // 'health': [],
+    // 'games-and-activities': [],
 }
 
 export const lists = (state = initState, action) => {
     switch(action.type) {
         case 'SET_LIST':
-            state[action.listName] = action.idNumbers;
-            return state
+            const newState = {...state};
+            newState[action.listName] = action.idNumbers;
+            return newState
         default:
             return state
     }
